68dbaa5e763652d1a9b9fe53c79a4d1fb89be0d4
Emilien Ordonneau <emilien.ordonneau@epfl.ch>
2025-11-26 18:50:31 +0100

    Added favicon and build.yaml for macos

--- packages/player-app/electron/main/index.ts
+++ packages/player-app/electron/main/index.ts
@@ -50,6 +50,7 @@ async function createWindow() {
     title: 'GM App',
     icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
     fullscreenable: true,
+    fullscreen: true,
     webPreferences: {
       preload,
       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
@@ -60,6 +61,8 @@ async function createWindow() {
       // contextIsolation: false,
     },
   })
+
+  win.maximize()
 
   if (VITE_DEV_SERVER_URL) { // #298
     win.loadURL(VITE_DEV_SERVER_URL)
